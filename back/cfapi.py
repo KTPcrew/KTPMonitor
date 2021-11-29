6a50be98ff6f6804cbab228b10b32a23799c987f
KTPCrew <catwepipe+tppo@gmail.com>
2021-11-29 16:11:33 +0300

    cf api update

--- back/cfapi.py
+++ back/cfapi.py
@@ -1,7 +1,20 @@
 import requests
 import pprint
+import hashlib
+import time
 
-pref = "https://codeforces.com/api/"
+pref = "https://codeforces.com/api"
+key, secret = (line.strip() for line in open("secret/cf cred.txt", "r"))
 
+def authorized_request(method, params):
+    params.append(("apiKey", key))
+    params.append(("time", str(int(time.time()))))
+    params.sort()
+    partial_request = method + '?' + '&'.join([par + '=' + val for par, val in params])
+    hash_line = "ktpmon/" + partial_request + "#" + secret
+    apiSig = hashlib.sha512(hash_line.encode()).hexdigest()
+    final_request = pref + '/' + partial_request + "&apiSig=ktpmon" + apiSig
+    print(final_request)
+    pprint.pprint(requests.get(final_request))
 
-pprint.pprint(requests.get(pref + "contest.status?contestId=566&from=1&count=10").json())
+authorized_request("contest.status", [("contestId", "344649")])
