fa5a0e943f85198a430281a3266871d66f42ba38
KTPCrew <catwepipe+tppo@gmail.com>
2021-12-14 12:12:46 +0300

    test

--- back/monitor.py
+++ back/monitor.py
@@ -7,6 +7,8 @@ delay = 60.0 * minutes_per_update
 divs = ['B', 'C', 'D']
 
 if __name__ == "__main__":
+    a = 0
+    print(5 / a)
     start = time.time()
     while True:
         for div in divs:
